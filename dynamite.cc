c2261774536686950aa6b9d8c829d3efd10e6021
Aleks Rutins <aleks@rutins.com>
2023-04-14 11:22:48 -0400

    Use the C++ version of the LV2 libraries

--- dynamite.cc
+++ dynamite.cc
@@ -1,42 +1,6 @@
 #include <lv2/core/lv2.h>
 #include "plugin.hh"
 
-using Dynamite::Plugin;
+using Dynamite::Drive;
 
-static LV2_Handle
-instantiate(const LV2_Descriptor*     descriptor,
-            double                    rate,
-            const char*               bundle_path,
-            const LV2_Feature* const* features)
-{
-    return new Plugin;
-}
-
-static void
-cleanup(LV2_Handle instance)
-{
-    delete (Plugin *)instance;
-}
-
-static const void*
-extension_data(const char *uri)
-{
-    return nullptr;
-}
-
-static const LV2_Descriptor descriptor = {
-    "https://github.com/aleksrutins/dynamite",
-    instantiate,
-    (void (*)(LV2_Handle, uint32_t, void*)) &Plugin::connect_port,
-    (void (*)(LV2_Handle)) &Plugin::activate,
-    (void (*)(LV2_Handle, uint32_t)) &Plugin::run,
-    (void (*)(LV2_Handle)) &Plugin::deactivate,
-    cleanup,
-    extension_data
-};
-
-LV2_SYMBOL_EXPORT
-const LV2_Descriptor*
-lv2_descriptor(uint32_t index) {
-    return index == 0 ? &descriptor : nullptr;
-}
+static int _ = Drive::register_class("https://github.com/aleksrutins/dynamite");
--- plugin.hh
+++ plugin.hh
@@ -1,19 +1,21 @@
 #pragma once
+#include <lv2plugin.hpp>
 #include <stdint.h>
+
+using namespace LV2;
 
 namespace Dynamite {
     enum PluginPort {
-        IN, OUT, DRIVE
+        IN, OUT, DRIVE, THRESHOLD
     };
 
-    class Plugin {
+    class Drive : public Plugin<Drive> {
         const float *drive;
         const float *input;
         float *output;
     public:
-        void connect_port(PluginPort port, void* data);
-        void activate();
+        Drive(double rate) : Plugin<Drive>(1) {}
+        void connect_port(uint32_t port, void* data);
         void run(uint32_t n_samples);
-        void deactivate();
     };
 }
